40d7d426649a0c6d32aa9c3390f83421c9afd8bf
Javajj <81207175+Javajj12@users.noreply.github.com>
2023-11-03 16:14:54 +0700

    login register
    login register

--- project/src/component/Register.jsx
+++ project/src/component/Register.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Form, Alert, Button, Image } from 'react-bootstrap';
-import { useUserAuth } from '../context/UserAuthContext'; //useContext
-import { db } from '../firebase'; //database
-import { collection, addDoc } from 'firebase/firestore'; //firestore
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Alert, Button, Image } from "react-bootstrap";
+import { useUserAuth } from "../context/UserAuthContext"; //useContext
+import { db } from "../firebase"; //database
+import { collection, addDoc } from "firebase/firestore"; //firestore
 import { Container, Row, Col } from "react-bootstrap";
-import RegisterCSS from "./style/Register..module.css"
+import RegisterCSS from "./style/Register.module.css";
 import img5 from "./image/img5.jpg";
-import img6 from "./image/img6.jpg"
-import img7 from "./image/img7.jpg"
-import img8 from "./image/img8.jpg"
-function Register() {
+import img6 from "./image/img6.jpg";
+import img7 from "./image/img7.jpg";
+import img8 from "./image/img8.jpg";
 
-  const [error, setError] = useState('');
+function Register() {
+  const [error, setError] = useState("");
   const { signUp } = useUserAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [phone_number, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
   const [product_id, setproduct_id] = useState([]);
   const [point, setpoint] = useState(0);
   const [qrtproduct_id, setqrtproduct_id] = useState([]);
@@ -26,24 +26,24 @@ function Register() {
   let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
-    setError('');
+    setError("");
 
     if (!email || !password || !username || !phone_number) {
-      setError('Please fill in all required fields.');
-      setLoading(false)
+      setError("Please fill in all required fields.");
+      setLoading(false);
       return;
     }
 
-    const cart_user = await addDoc(collection(db, 'cart'), {
+    const cart_user = await addDoc(collection(db, "cart"), {
       product_id: product_id,
       email: email,
       qauntityPerProductID: qrtproduct_id,
     });
 
     try {
-      const userDocRef_Users = await addDoc(collection(db, 'user'), {
+      const userDocRef_Users = await addDoc(collection(db, "user"), {
         email: email,
         username: username,
         phone_number: phone_number,
@@ -51,10 +51,10 @@ function Register() {
       });
 
       await signUp(email, password);
-      navigate('/');
+      navigate("/");
     } catch (err) {
       setError(err.message);
-      setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -63,35 +63,7 @@ function Register() {
       <br />
       <br />
       <Row>
-        <Col> <Row>
-          <Col className="size" md={5}>
-            <Row>
-              <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                <div className="aspect-ratio-box">
-                  <Image src={img5} alt="Image 1" className="resize" />
-                </div>
-              </Col>
-              <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                <div className="aspect-ratio-box">
-                  <Image src={img6} alt="Image 2" className="resize" />
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                <div className="aspect-ratio-box">
-                  <Image src={img7} alt="Image 3" className="resize" />
-                </div>
-              </Col>
-              <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                <div className="aspect-ratio-box">
-                  <Image src={img8} alt="Image 4" className="resize" />
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row></Col>
-        <Col>
+        <Col md={6}>
           <Row>
             <Col className={RegisterCSS.block_2}>
               <br />
@@ -104,12 +76,8 @@ function Register() {
               {error && <Alert variant="danger">{error}</Alert>}
 
               <Form onSubmit={handleSubmit}>
-
                 {/* form email */}
-                <Form.Group
-                  className="mb-3"
-                  controlId="formBasicEmail"
-                >
+                <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control
                     type="email"
@@ -119,7 +87,6 @@ function Register() {
                   />
                 </Form.Group>
 
-                {/* form username */}
                 <Form.Group className="mb-3">
                   <Form.Label>Username</Form.Label>
                   <Form.Control
@@ -139,15 +106,12 @@ function Register() {
                     placeholder="Phone number"
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     className={RegisterCSS.input_small}
-                    pattern='^0{1}[6-9]{1}[0-9]{8}'
+                    pattern="^0{1}[6-9]{1}[0-9]{8}"
                   />
                 </Form.Group>
 
                 {/* form password */}
-                <Form.Group
-                  className="mb-3"
-                  controlId="formBasicPassword"
-                >
+                <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
                     type="password"
@@ -158,8 +122,13 @@ function Register() {
                 </Form.Group>
 
                 <div className="d-grid gap-2 justify-content-center">
-                  <Button variant="warning" className="custom-button-style" type="submit" disabled={isLoading}  >
-                    {isLoading ? 'Loading…' : 'SignUp'}
+                  <Button
+                    variant="warning"
+                    className={RegisterCSS.custom_button_style}
+                    type="submit"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "Loading…" : "SignUp"}
                   </Button>
                 </div>
               </Form>
@@ -167,12 +136,58 @@ function Register() {
               <div className="p-4 box mt-3 text-center">
                 Already have an account? <Link to="/login">Login</Link>
               </div>
-
             </Col>
           </Row>
         </Col>
-      </Row >
-    </Container >
+
+        <Col md={6}>
+          <Row style={{ margin: "60px" }} className={RegisterCSS.image_resize}>
+            <Col className={RegisterCSS.size} md={5}>
+              <Row>
+                <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                  <div className={RegisterCSS.aspect_ratio_box}>
+                    <Image
+                      src={img5}
+                      alt="Image 1"
+                      className={RegisterCSS.resize}
+                    />
+                  </div>
+                </Col>
+                <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                  <div className={RegisterCSS.aspect_ratio_box}>
+                    <Image
+                      src={img6}
+                      alt="Image 2"
+                      className={RegisterCSS.resize}
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                  <div className={RegisterCSS.aspect_ratio_box}>
+                    <Image
+                      src={img7}
+                      alt="Image 3"
+                      className={RegisterCSS.resize}
+                    />
+                  </div>
+                </Col>
+                <Col md={6} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                  <div className={RegisterCSS.aspect_ratio_box}>
+                    <Image
+                      src={img8}
+                      alt="Image 4"
+                      className={RegisterCSS.resize}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default Register;
